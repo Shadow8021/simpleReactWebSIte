f2ab0d325551a4ee8862d445355ded1578e10683
Sh@dow <gregoyaga@gmail.com>
2025-10-30 15:45:18 +0100

    finalisation

--- src/components/Servicess/Services.jsx
+++ src/components/Servicess/Services.jsx
@@ -1,14 +1,12 @@
 import { datas } from "./man"
 import "./services.css"
-// import image from "../../assets/images/services/img3.png"
-
 
 export const Services = () => {
   return (
     <div className='services'>
       <h2 className="serviceH">Nos Services</h2>
       {datas.map((data) => (
-        <div className={data.id % 2 == 0 ? `serviceCard` : "serviceCard2"}>
+        <div key={data.id} className={data.id % 2 == 0 ? `serviceCard` : "serviceCard2"}>
           <div className="serviceInfo">
             <h2 className="titreService">{data.titre}</h2>
             <p className="sousTitre">{data.sousTitre}</p>
@@ -23,18 +21,3 @@ export const Services = () => {
 
   );
 }
-
-
-
-
-// <div className='serviceCard'>
-//   <div >
-//     <h2> Developpement web et mobile</h2>
-//     <p>Creation des site vitrines, e-commerces,web apps</p>
-//     <p>Applications Android/iOS</p>
-//     <p>Maintenance et hébergement</p>
-//   </div>
-//   <div className='serviceImageContainer'>
-//     <img src={image} alt="logo" className='serviceImage' />
-//   </div>
-// </div>
