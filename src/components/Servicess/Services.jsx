7e90518d8d58d905def0c9e368c1349519c6606b
shadow <gregoyaga@gmail.com>
2025-10-31 18:54:04 +0100

    finalisation de la section about

--- src/components/Servicess/Services.jsx
+++ src/components/Servicess/Services.jsx
@@ -6,7 +6,7 @@ export const Services = () => {
     <div className='services'>
       <h2 className="serviceH">Nos Services</h2>
       {datas.map((data) => (
-        <div key={data.id} className={data.id % 2 == 0 ? `serviceCard` : "serviceCard2"}>
+        <div key={data.id} className={data.id % 2 == 0 ? `serviceCard` : "serviceCard serviceCard2"}>
           <div className="serviceInfo">
             <h2 className="titreService">{data.titre}</h2>
             <p className="sousTitre">{data.sousTitre}</p>
