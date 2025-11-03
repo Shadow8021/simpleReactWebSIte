7005f3a4e0f8906daea2ebed91e61d302465e1d3
shadow <gregoyaga@gmail.com>
2025-11-03 13:07:05 +0100

    fin de la configuration de l'objet donnees dans partenaires.jsx

--- src/assets/images/partenaire/partenaires.jsx
+++ src/assets/images/partenaire/partenaires.jsx
@@ -3,3 +3,20 @@ import comptia from "./Comptialogo.png"
 import tcm from "./tcmlogo.png"
 import epitech from "./220px-Epitech.png"
 //importation des images
+
+export let donnees = [
+    {
+        id: 0,
+        image: google
+    }, {
+        id: 1,
+        image: tcm
+    },
+    {
+        id: 2,
+        image: epitech
+    }, {
+        id: 3,
+        image: comptia
+    }
+]
