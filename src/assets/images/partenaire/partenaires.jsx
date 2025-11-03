b82b6bef10396a78eb58e43d44b33b01c7372bae
shadow <gregoyaga@gmail.com>
2025-11-03 13:01:26 +0100

    importation des image dans partenaires.jsx

--- src/assets/images/partenaire/partenaires.jsx
+++ src/assets/images/partenaire/partenaires.jsx
@@ -1,1 +1,5 @@
 import google from "./googllogo.svg.png"
+import comptia from "./Comptialogo.png"
+import tcm from "./tcmlogo.png"
+import epitech from "./220px-Epitech.png"
+//importation des images
